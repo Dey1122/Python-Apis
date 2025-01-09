dc4a7cdcb93f3e584e9a4ea3ccc25599093f3120
Dey1122 <145286230+Dey1122@users.noreply.github.com>
2025-01-09 00:47:42 -0500

    metodoGet

--- patients/views.py
+++ patients/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .serializers import PatientSerializer
+from .models import Patient
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view(["GET"])
+def list_patients(request):
+    patiens = Patient.objects.all()
+    serializer = PatientSerializer(patiens, many=True)
+    return Response(serializer.data)
